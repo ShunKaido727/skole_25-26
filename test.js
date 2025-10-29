4585a1b3c345b13cf3524477e06bdb74eb9ea294
Your Name <your.email@example.com>
2025-10-29 14:13:57 -0200

    javascript matte

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log('hei jeg lærer meg JavaScript!');
+
+let antall = 5;
+let ganger = 10;
+const test = 33;
+let sum = (antall + ganger*test);
+console.log(sum);
+
+let navn = 'Lupus';
+console.log(navn);
+//kommentar
+
+/*test
+test
+test*/
